e9d076a2449a40532e4f02b948a2a284e8514d89
Alexcitten <61086853+Alexcitten@users.noreply.github.com>
2025-02-06 16:58:01 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,65 +47,96 @@
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function(e) {
-    const navbar = select('#navbar');
-    if (navbar) {
-      navbar.classList.toggle('navbar-mobile');
-      this.classList.toggle('bi-list');
-      this.classList.toggle('bi-x');
-    }
-  });
+on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
 
   /**
-   * Scroll with offset on links with .scrollto class
+   * Scrool with ofset on links with a class name .scrollto
    */
   on('click', '#navbar .nav-link', function(e) {
-    if (this.hash) {
-      e.preventDefault();
-      const section = select(this.hash);
-      
-      if (section) {
-        let navbar = select('#navbar');
-        let navlinks = select('#navbar .nav-link', true);
-        navlinks.forEach((item) => item.classList.remove('active'));
+    let section = select(this.hash)
+    if (section) {
+      e.preventDefault()
 
-        this.classList.add('active');
+      let navbar = select('#navbar')
+      let header = select('#header')
+      let sections = select('section', true)
+      let navlinks = select('#navbar .nav-link', true)
 
-        if (navbar.classList.contains('navbar-mobile')) {
-          navbar.classList.remove('navbar-mobile');
-          let navbarToggle = select('.mobile-nav-toggle');
-          navbarToggle.classList.toggle('bi-list');
-          navbarToggle.classList.toggle('bi-x');
-        }
+      navlinks.forEach((item) => {
+        item.classList.remove('active')
+      })
 
-        scrollto(this.hash);
+      this.classList.add('active')
 
-        if (history.pushState) {
-          history.pushState(null, null, this.hash);
-        } else {
-          window.location.hash = this.hash;
-        }
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
       }
+
+      if (this.hash == '#header') {
+        header.classList.remove('header-top')
+        sections.forEach((item) => {
+          item.classList.remove('section-show')
+        })
+        return;
+      }
+
+      if (!header.classList.contains('header-top')) {
+        header.classList.add('header-top')
+        setTimeout(function() {
+          sections.forEach((item) => {
+            item.classList.remove('section-show')
+          })
+          section.classList.add('section-show')
+
+        }, 350);
+      } else {
+        sections.forEach((item) => {
+          item.classList.remove('section-show')
+        })
+        section.classList.add('section-show')
+      }
+
+      scrollto(this.hash)
     }
-  }, true);
+  }, true)
 
   /**
    * Activate/show sections on load with hash links
    */
   window.addEventListener('load', () => {
     if (window.location.hash) {
-      let initial_nav = select(window.location.hash);
+      let initial_nav = select(window.location.hash)
 
       if (initial_nav) {
-        let navlinks = select('#navbar .nav-link', true);
-        navlinks.forEach((item) => {
-          item.classList.toggle('active', item.getAttribute('href') === window.location.hash);
-        });
+        let header = select('#header')
+        let navlinks = select('#navbar .nav-link', true)
 
-        scrollto(window.location.hash);
+        header.classList.add('header-top')
+
+        navlinks.forEach((item) => {
+          if (item.getAttribute('href') == window.location.hash) {
+            item.classList.add('active')
+          } else {
+            item.classList.remove('active')
+          }
+        })
+
+        setTimeout(function() {
+          initial_nav.classList.add('section-show')
+        }, 350);
+
+        scrollto(window.location.hash)
       }
     }
   });
+
 
   /**
    * Skills animation
