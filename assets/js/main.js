3b99f0f0bf663b04835b97022afd48242dc1260c
Alexcitten <61086853+Alexcitten@users.noreply.github.com>
2025-02-06 17:37:32 +0200

    Update main.js
    cringe

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,6 +29,10 @@
     }
   };
 
+  const smoothScrollTo = (el) => {
+    el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  };
+
   /**
    * Mobile nav toggle
    */
@@ -41,7 +45,43 @@
   /**
    * Activate/show sections on load with hash links
    */
-  window.addEventListener('load', () => {
+ on('click', '#navbar .nav-link', function(e) {
+    let section = select(this.getAttribute('href'));
+
+    if (section) {
+      e.preventDefault();
+
+      let navbar = select('#navbar');
+      let header = select('#header');
+      let sections = select('section', true);
+      let navlinks = select('#navbar .nav-link', true);
+
+      navlinks.forEach((item) => item.classList.remove('active'));
+      this.classList.add('active');
+
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile');
+        let navbarToggle = select('.mobile-nav-toggle');
+        navbarToggle.classList.toggle('bi-list');
+        navbarToggle.classList.toggle('bi-x');
+      }
+
+      if (!header.classList.contains('header-top')) {
+        header.classList.add('header-top');
+        setTimeout(() => {
+          sections.forEach(item => item.classList.remove('section-show'));
+          section.classList.add('section-show');
+        }, 350);
+      } else {
+        sections.forEach(item => item.classList.remove('section-show'));
+        section.classList.add('section-show');
+      }
+
+      smoothScrollTo(section);
+    }
+  }, true);
+
+    window.addEventListener('load', () => {
     let initial_nav = select(window.location.hash);
     if (initial_nav) {
       let header = select('#header');
@@ -56,9 +96,11 @@
       setTimeout(() => {
         initial_nav.classList.add('section-show');
       }, 350);
+
+      smoothScrollTo(initial_nav);
     }
   });
-
+  
   /**
    * Skills animation
    */
